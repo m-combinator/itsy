beb04c6625bbeaff2fc69ddcf08f336d7393593d
m-combinator <mostafa.gomaa89@gmail.com>
2015-07-06 05:23:22 +0200

    Adds extract-urls function that uses tika to extract urls from web page.

--- src/itsy/extract.clj
+++ src/itsy/extract.clj
@@ -3,7 +3,9 @@
            (org.apache.tika.sax BodyContentHandler)
            (org.apache.tika.metadata Metadata)
            (org.apache.tika.parser ParseContext)
-           (org.apache.tika.parser.html HtmlParser)))
+           (org.apache.tika.parser.html HtmlParser)
+           (org.apache.tika.sax LinkContentHandler)
+           (java.nio.charset StandardCharsets)))
 
 (defn html->str
   "Convert HTML to plain text using Apache Tika"
@@ -15,3 +17,17 @@
           parser (HtmlParser.)]
       (.parse parser bais handler metadata (ParseContext.))
       (.toString handler))))
+
+(defn extract-urls
+  "extract all links from webpage. returns a seq of urls"
+  [body]
+  (when body
+    (let [link-handler (new LinkContentHandler)
+          html-parser (new HtmlParser)
+          parse-context (new ParseContext)
+          metadata (new Metadata)
+          inputstream (new ByteArrayInputStream (. body
+                                                   getBytes
+                                                   (StandardCharsets/UTF_8)))]
+      (. html-parser parse inputstream link-handler metadata parse-context)
+      (map #(. % getUri) (. link-handler getLinks)))))
